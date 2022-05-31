1ad443356f5a307ed438997f5acb3881a87733ce
syunatsu <myf.tyys@gmail.com>
2022-05-31 15:23:27 +0900

    切り上げ、切り捨て、四捨五入、絶対値、冪乗

---
+++ 切り捨て、切り上げ、四捨五入、絶対値、冪乗/main.js
@@ -0,0 +1,17 @@
+const number = 1.534
+
+//切り捨て
+console.log(Math.floor(number));
+console.log(Math.trunc(number));
+
+//切り上げ
+console.log(Math.ceil(number));
+
+//四捨五入
+console.log(Math.round(number));
+
+// 絶対値
+console.log(Math.abs(number));
+
+//冪乗
+console.log(Math.pow(number, 3));
