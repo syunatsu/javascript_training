81ab24e85967968f6703eb54633408c698d3c7ac
syunatsu <myf.tyys@gmail.com>
2022-06-01 19:08:26 +0900

    アロー関数

---
+++ アロー関数/main.js
@@ -0,0 +1,16 @@
+// function tax(number) {
+//   return number * 1.08;
+// };
+
+// const tax = function (number){
+//   return number * 1.08;
+// };
+
+// const tax = (number) => {
+//   return number * 1.08;
+// }
+// document.getElementById('test').textContent = tax(100);
+
+document.getElementById('test').addEventListener('click', function () {
+  this.textContent = 'クリックされました！';
+});
